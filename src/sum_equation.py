8c690cb18afb30480489c87cb796c2be900ee786
modengann <99417989+modengann@users.noreply.github.com>
2022-08-29 11:49:54 -0400

    Update sum_equation.py

--- src/sum_equation.py
+++ src/sum_equation.py
@@ -1,20 +1,10 @@
 #!/usr/bin/env python3
 
 def sum_equation(L):
-    temp = []
-    return_string = ""
-    if len(L) > 0:  
-        for i in L:
-            temp.append(str(i))
-        return_string += " + ".join(temp)
-        return_string += f" = {sum(L)}"
-    else:
-        return_string = "0 = 0"
-    return return_string
+    pass
 
 def main():
-    L = []
-    sum_equation(L)
+    pass
 
 if __name__ == "__main__":
     main()
